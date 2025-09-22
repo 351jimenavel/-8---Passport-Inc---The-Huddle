0871e2a7868e00063632e3ccc4cfa81e2a463aad
Jimena Velázquez <jimena.avelazquezlez@gmail.com>
2025-09-22 00:18:06 -0300

    Agregar rutas basicas

--- app.js
+++ app.js
@@ -5,6 +5,13 @@ const express = require('express');
 const app = express()
 const port = 8080;
 
+// Rutas
+app.post('/login', (req, res) => {})
+app.post('/register', (req, res) => {})
+app.post('/logout', (req, res) => {})
+
+app.get('/protected', (req, res) => {})
+
 // Levantar servidor
 app.listen(port, () => {
     console.log("Servidor corriendo en puerto", `http://localhost:${port}`);
