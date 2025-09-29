2a6234e5f5d507a14d7260bf5f35222587ef187b
Jimena Velázquez <jimena.avelazquezlez@gmail.com>
2025-09-29 19:26:53 -0300

    Arreglar importacion de modulos + agregar logica de cookie

--- app.js
+++ app.js
@@ -1,12 +1,18 @@
 // Configuracion basica
-const express = require('express');
-const { UserRepository } = require('./src/models/user');
+//const express = require('express');
+import express from 'express';
+import jwt from 'jsonwebtoken';
+import cookieParser from 'cookie-parser';
+import { PORT, SECRET_JWT_KEY } from './config.js';
+//const jwt = require('jsonwebtoken');
+import { UserRepository } from './src/models/user.js';
+//const { UserRepository } = require('./src/models/user');
 
 // Inicializar app
 const app = express()
 app.set('view engine', 'ejs');
 app.use(express.json())
-const port = 8080;
+app.use(cookieParser())
 
 app.get('/', (req,res) => {
     res.render('index')
@@ -22,7 +28,17 @@ app.post('/login', async (req, res) => {
     const { username, password } = req.body;
     try{
         const user = await UserRepository.login( { username, password });
-        res.send( { user });
+        const token = jwt.sign({ id: user._id, username: user.username }, 
+            SECRET_JWT_KEY, 
+            {
+            expiresIn: '1h'
+            })
+        res.cookie('access_token', token, { httpOnly: true,   // la cookie solo se puede acceder en el servidor
+        sameSite: 'lax',    // 
+        secure: false,      // 
+        maxAge: 1000 * 60 * 60  // la cookie tiene un tiempo de validez de 1h
+        })
+        res.send( { user, token });
     }catch(error){
         // NORMALMENTE NO ES LO IDEAL MANDAR EL ERROR DEL REPOSITORIO (porque puede tener informacion por de mas)
         res.status(401).send(error.message)
@@ -44,10 +60,12 @@ app.post('/register', async (req, res) => {
 app.post('/logout', (req, res) => {})
 
 app.get('/protected', (req, res) => {
-    res.render('')
+    // TODO: if sesion del usuario
+    res.render('protected', {username: 'jimenaa'})
+    // TODO: else 401
 })
 
 // Levantar servidor
-app.listen(port, () => {
-    console.log("Servidor corriendo en puerto", `http://localhost:${port}`);
+app.listen(PORT, () => {
+    console.log("Servidor corriendo en puerto", `http://localhost:${PORT}`);
 });
