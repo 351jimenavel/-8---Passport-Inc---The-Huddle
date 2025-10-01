dd7bb8801f30fea22878cb91f28305bf263b6ec8
Jimena Velázquez <jimena.avelazquezlez@gmail.com>
2025-09-30 22:18:32 -0300

    Actualizar estructura

--- app.js
+++ app.js
@@ -7,13 +7,13 @@ import helmet from 'helmet';
 import { PORT, SECRET_JWT_KEY, SESSION_SECRET } from './config.js';
 import { UserRepository } from './src/models/user.js';
 import { randomBytes, randomUUID } from 'node:crypto';
-import db from './src/db.js'
+
 import authRoutes from './src/routes/auth.routes'
 import adminRoutesCookie from './src/routes/admin.routes'           // versión cookie
 // import adminRoutesJWT from './src/routes/admin.routes.jwt'       // si prefieres JWT
 
 // Inicializar app
-const app = express()
+const app = express();
 app.set('view engine', 'ejs');
 app.use(express.json())
 app.use(cookieParser())
@@ -33,23 +33,6 @@ app.use(session({
     }
 }))
 
-// ----- Autenticacion por JWT en cada request (para vistas)
-//// Middleware Leer JWT desde Cookie
-app.use((req, res, next) => {
-    const token = req.cookies.access_token;
-    req.sessionJwtUser = null;
-
-    if (token){
-        try {
-            const data = jwt.verify(token, SECRET_JWT_KEY);
-            req.sessionJwtUser = data;  // { id, username, role }
-        } catch{
-            req.sessionJwtUser = null;
-        }
-    }
-    next();     // seguir a la siguiente ruta o middleware
-})
-
 app.get('/', (req,res) => {
     // si hay sesion cookie, usarla; si no, probar JWT; si no, vacio
     const user = req.session?.user || req.sessionJwtUser || null;
@@ -61,16 +44,6 @@ app.get('/', (req,res) => {
 //     res.render('index', {username: "jimena"})
 // })
 
-// Auth endpoints
-app.post('/register', )
-
-app.post('/login', );
-
-// rama JWT
-app.post('/token/refresh', )
-
-app.post('/logout', );
-
 app.get('/protected', (req, res) => {
     const user = req.session?.user || req.sessionJwtUser;
     if (!user){
@@ -78,6 +51,10 @@ app.get('/protected', (req, res) => {
     }
     return res.render('protected', user);
 })
+
+// Montar rutas modulares
+app.use(authRoutes);        // /register, /login, /logout, /token/refresh
+app.use(adminRoutesCookie); // /admin/users (requiere rol ADMIN)
 
 // Levantar servidor
 app.listen(PORT, () => {
