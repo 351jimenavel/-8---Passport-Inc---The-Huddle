6c07ec94ed5892a86dbf9a2cb13e7b34017d6599
Jimena Velázquez <jimena.avelazquezlez@gmail.com>
2025-10-09 20:50:04 -0300

    fix: exportar correctamente

--- src/middlewares/verifyJWT.js
+++ src/middlewares/verifyJWT.js
@@ -1,22 +1,20 @@
 import jwt from 'jsonwebtoken';
 import { SECRET_JWT_KEY} from '../../config.js';
 
-function verifyJWT(req, res, next){
+export default function verifyJWT(req, res, next){
     const auth = req.headers.authorization || '';
 
     if (!auth.startsWith('Bearer ')){
         return res.status(401).send('Unauthorized');
     }
 
-    const token = auth.slice(7).trim(); // corta "Bearer "
+    const token = auth.slice('Bearer '.length).trim(); // corta "Bearer "
 
     try{
         const payload = jwt.verify(token, SECRET_JWT_KEY);  // { sub, role, username }
         req.user = { id: payload.sub, role: payload.role, username: payload.username };
         next();
     }catch{
-        return res.status(401).send('Unauthorized');
+        res.status(401).send('Unauthorized');
     }
 }
-
-export default verifyJWT
