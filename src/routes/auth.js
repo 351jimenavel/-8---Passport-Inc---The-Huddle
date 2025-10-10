c96c4e578d96005ee3626b9fbaeb65040f0dde39
Jimena Velázquez <jimena.avelazquezlez@gmail.com>
2025-10-09 23:39:49 -0300

    fix: mensajes de error mas simples

--- src/routes/auth.js
+++ src/routes/auth.js
@@ -17,7 +17,7 @@ const validateCreds = [
     (req, res, next) => {
         const errors = validationResult(req);
         if (!errors.isEmpty()) {
-            return res.status(400).json({ errors: errors.array() })
+            return res.status(400).json({ message: 'Invalid credentials format' })
         };
         next();
     }
