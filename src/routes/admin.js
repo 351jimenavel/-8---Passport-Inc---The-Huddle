a0cb445e2359b68e9df1063f193e68c8c443d3f7
Jimena Velázquez <jimena.avelazquezlez@gmail.com>
2025-10-09 23:08:50 -0300

    feat: ruta que renderiza vista con usuarios

--- src/routes/admin.js
+++ src/routes/admin.js
@@ -4,6 +4,13 @@ const router = express.Router();
 import requireAuthCookie from '../middlewares/requireAuthCookie.js'
 import requireRole from '../middlewares/requireRole.js'
 import validateCSRF from '../middlewares/validateCSRF.js'
+import { UserRepository } from '../models/user.js';
+
+router.get('/admin', requireAuthCookie, requireRole('ADMIN', (req, res) => {
+    const users = UserRepository.listAll();
+    const csrf = req.session?.csrfToken || '';
+    res.render('admin', {users, csrf });
+}))
 
 router.get('/admin/users', requireAuthCookie, requireRole('ADMIN'), listUsers);
 router.delete('/admin/users/:id', requireAuthCookie, requireRole('ADMIN'), validateCSRF ,deleteUser);
